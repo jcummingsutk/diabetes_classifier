e94e4e70757249e24a57850038511e6cb7091a8e
John Cummings <jcummingsutk@gmail.com>
2023-12-27 21:38:57 -0500

    cred

--- cicd/create_training_environment.py
+++ cicd/create_training_environment.py
@@ -2,18 +2,18 @@ import os
 
 from azure.ai.ml import MLClient
 from azure.ai.ml.entities import BuildContext, Environment
+from azure.identity import DefaultAzureCredential
 
 if __name__ == "__main__":
     subscription_id = os.environ["AZURE_ML_SUBSCRIPTION_ID"]
     resource_group_name = os.environ["AZURE_ML_RESOURCE_GROUP_NAME"]
     workspace_name = os.environ["AZURE_ML_WORKSPACE_NAME"]
-    cred = os.environ["SERVICE_PRINCIPAL_CLIENT_SECRET"]
 
     ml_client = MLClient(
         subscription_id=subscription_id,
         resource_group_name=resource_group_name,
         workspace_name=workspace_name,
-        credential=cred,
+        credential=DefaultAzureCredential(),
     )
     build = BuildContext(path="./docker_build_context/")
     env_docker_context = Environment(build=build, name="diabetes-classifier-staging")
